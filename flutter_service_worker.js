98da950067246fee84994fa5d4c7a094c6577ed8
xvadim12 <vadim.khohlov@postindustria.com>
2021-03-29 12:15:22 +0300

    upd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "16cb1b694c06c5f83e78301688ffade4",
-"index.html": "d2a1667936a2d31d36528bc48ad4a2e4",
-"/": "d2a1667936a2d31d36528bc48ad4a2e4",
-"main.dart.js": "79e634210c19f0a3e7fca3d9e0f036cf",
+"index.html": "811e9cd07708f5dc9d9f8853c7639042",
+"/": "811e9cd07708f5dc9d9f8853c7639042",
+"main.dart.js": "2cdcd4a2a70ec26738e5e5185f1402c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
